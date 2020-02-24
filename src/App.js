7af63d0491cc98b79e0c8cbe250e279f756053c0
louisr133 <louisroman133@gmail.com>
2020-02-24 00:33:15 -0500

    changes to app name

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Compression from "./Components /Compression";
 import Similar from "./Components /Similar";
 import SideBar from "./Components /SideBar";
 import { BrowserRouter, Route } from 'react-router-dom';
-import './App.css ';
+import './App.css';
 
 
 
